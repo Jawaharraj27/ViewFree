d73ac8e0770e4efea97ed59f5e4647cbc7b8a4cf
jawahar raj <jawaharrajns27@gmail.com>
2026-07-20 17:15:33 +0530

    v0.3.1 - Added air click animation

--- src/ui/view_orb.py
+++ src/ui/view_orb.py
@@ -4,13 +4,23 @@ import cv2
 def draw_view_orb(frame, x, y, pinch=False):
 
     if pinch:
+        outer_radius = 20
+        inner_radius = 12
+        outer_color = (255, 255, 255)
+        inner_color = (0, 255, 255)
+
+        # Flash Ring
+        cv2.circle(
+            frame,
+            (x, y),
+            35,
+            (255, 255, 255),
+            2
+        )
+
+    else:
         outer_radius = 28
         inner_radius = 18
-        outer_color = (0, 255, 255)
-        inner_color = (0, 180, 255)
-    else:
-        outer_radius = 22
-        inner_radius = 14
         outer_color = (255, 180, 0)
         inner_color = (255, 120, 0)
 
